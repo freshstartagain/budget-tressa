609ab673baa1c3bf51b11a7325c053db5d67e558
lesterBC <lester.cayabyab@gopassglobal.com>
2021-06-26 19:24:07 +0800

    refactor: Fixed the env for docker deployment

--- instance/config.py
+++ instance/config.py
@@ -1,29 +1,25 @@
-from os import environ, path
-from dotenv import load_dotenv
+import os
 from datetime import timedelta
-
-basedir = path.abspath(path.dirname(__file__))
-load_dotenv(path.join(basedir, '.env'))
 
 class Config:
      DEBUG = False
      CSRF_ENABLE = True
-     SECRET_KEY = environ.get('SECRET_KEY')
+     SECRET_KEY = os.environ['SECRET_KEY']
 
      SQLALCHEMY_ECHO = False
      SQLALCHEMY_TRACK_MODIFICATIONS = False   
      
-     JWT_SECRET_KEY = environ.get('JWT_SECRET_KEY')
+     JWT_SECRET_KEY = os.environ['JWT_SECRET_KEY']
      JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
 
 class DevelopmentConfig(Config):
      DEBUG = True
-     SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{environ.get('DATABASE_USER')}:{environ.get('DATABASE_PASSWORD')}@db:{environ.get('DATABASE_HOST')}/{environ.get('DATABASE_DB_DEV')}"
+     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_DB_DEV_URI']
 
 class TestingConfig(Config):
      TESTING = True
      DEBUG = True
-     SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{environ.get('DATABASE_USER')}:{environ.get('DATABASE_PASSWORD')}@db:{environ.get('DATABASE_HOST')}/{environ.get('DATABASE_DB_TEST')}"
+     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_DB_TEST_URI']
 
 class StagingConfig(Config):
      DEBUG = True
